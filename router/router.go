e7ca6f7b17cd5b46c1dfb98662425ad61a31ad37
huhangbo <1746982699@qq.com>
2021-10-09 15:17:15 +0800

    优化

--- router/router.go
+++ router/router.go
@@ -3,24 +3,19 @@ package router
 import (
 	"Moreover/setting"
 	"github.com/gin-gonic/gin"
-	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"io"
-	"time"
+	"os"
 )
 
 var Router *gin.Engine
 
 func InitRouter(port string) {
-	gin.SetMode(setting.Config.Mode)
-	logPath := setting.Config.LogConfig.Path
-	writer, _ := rotatelogs.New(
-		logPath+"%Y-%m-%d.log",
-		rotatelogs.WithLinkName(logPath),
-		rotatelogs.WithMaxAge(time.Duration(180)*time.Second),
-		rotatelogs.WithRotationTime(time.Duration(60)*time.Minute*24),
-	)
 
-	gin.DefaultWriter = io.MultiWriter(writer)
+	gin.SetMode(setting.Config.Mode)
+
+	f, _ := os.Create("log/gin.log")
+
+	gin.DefaultWriter = io.MultiWriter(f)
 
 	Router = gin.Default()
 
