9ee41b38e6ccd5a646af2ce7ce045b05d61e8793
huhangbo <1746982699@qq.com>
2022-01-18 15:59:14 +0800

    背景图片

--- dao/userInfo.go
+++ dao/userInfo.go
@@ -14,7 +14,7 @@ type UserInfo struct {
 	Nickname    string                `gorm:"default:取个名字吧" json:"nickname"`
 	Avatar      string                `gorm:"default:https://moreover-1305054989.cos.ap-nanjing.myqcloud.com/author.jpg" json:"avatar"`
 	Sex         string                `gorm:"default:未知" json:"sex"`
-	Background  string                `gorm:"https://huhangbo.gitee.io/huhangbo-imgs/CUMT/3.JPG" json:"background"`
+	Background  string                `gorm:"default:http://www.cumt.edu.cn/_upload/article/images/e6/c7/4bd9beeb4438899f538acd1d3360/6a204d99-cff9-41a6-8f09-6dbdd6d34f13.jpg" json:"background"`
 	Description string                `gorm:"default:添加一句话描述自己吧" json:"description"`
 }
 
